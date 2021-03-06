99077af56228ab920e9d5898d183a9dbe96066d1
ahmad1091 <ahmadtayeb91@gmail.com>
2021-03-06 22:01:41 +0200

    adding the updated files

---
+++ MyToken.js
@@ -0,0 +1,18 @@
+const MyToken = artifacts.require("./MyToken");
+
+contract("MyToken", (accounts) => {
+  it("set the total supply upon deployment", () => {
+    return MyToken.deployed()
+      .then((instance) => {
+        tokenInstance = instance;
+        return tokenInstance.totalSupply();
+      })
+      .then((totalSupply) => {
+        assert.equal(
+          totalSupply.toNumber(),
+          1000000,
+          "sets the total supply to 1,000,000"
+        );
+      });
+  });
+});
--- contracts/my_token.sol
+++ contracts/my_token.sol
@@ -1,2 +1,10 @@
-//SPDX-License-Identifier:MIT
-pragma solidity ^0.7.4;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract MyToken {
+    uint256 public totalSupply;
+
+    constructor() public {
+        totalSupply = 1000000;
+    }
+}
