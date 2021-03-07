1a1b17b0d547fc15f42f921e428c197663208f53
ahmad1091 <ahmadtayeb91@gmail.com>
2021-03-07 21:01:41 +0200

    transferFrom function

--- contracts/my_token.sol
+++ contracts/my_token.sol
@@ -50,8 +50,18 @@ contract MyToken {
     }
 
     function transferFrom(
-        address _form,
+        address _from,
         address _to,
         uint256 _value
-    ) public returns (bool success) {}
+    ) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
 }
