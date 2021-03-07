2149f3cf45167d4aee180cb206e834abd148c8f1
ahmad1091 <ahmadtayeb91@gmail.com>
2021-03-07 09:10:34 +0200

    createing new test for checking the contact public variables

--- test/MyToken.js
+++ test/MyToken.js
@@ -1,17 +1,40 @@
 const MyToken = artifacts.require("./MyToken.sol");
 
 contract("MyToken", (accounts) => {
+  it("initialize token name and symbol", () => {
+    return MyToken.deployed()
+      .then((instance) => {
+        tokenInstance = instance;
+        return tokenInstance.name();
+      })
+      .then((name) => {
+        assert.strictEqual(name, "MyToken", "has the correct name");
+        return tokenInstance.symbol();
+      })
+      .then((symbol) => {
+        assert.strictEqual(symbol, "MYN");
+      });
+  });
+
   it("sets the total supply", () => {
     return MyToken.deployed()
-      .then((instace) => {
-        tokenInstance = instace;
+      .then((instance) => {
+        tokenInstance = instance;
         return (totalSupply = tokenInstance.totalSupply());
       })
       .then((totalSupply) => {
         assert.strictEqual(
           totalSupply.toNumber(),
           1000000,
-          "total supply equal to 1000000"
+          "total supply equal to 1,000,000"
+        );
+        return tokenInstance.balanceOf(accounts[0]);
+      })
+      .then((adminBalance) => {
+        assert.strictEqual(
+          adminBalance.toNumber(),
+          1000000,
+          "initial admin balance equale to the total supply  1,000,000"
         );
       });
   });
