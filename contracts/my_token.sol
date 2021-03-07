7939220d27a215ff5f040333db30c05b0ab86c84
ahmad1091 <ahmadtayeb91@gmail.com>
2021-03-07 13:59:10 +0200

    create allowance function

--- contracts/my_token.sol
+++ contracts/my_token.sol
@@ -8,8 +8,13 @@ contract MyToken {
     uint256 public totalSupply;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _amount
+    );
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(
         uint256 _initialSupply,
@@ -34,5 +39,13 @@ contract MyToken {
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
+
+    function approve(address _spender, uint256 _amount)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _amount;
+        emit Approval(msg.sender, _spender, _amount);
+        return true;
+    }
 }
-    
