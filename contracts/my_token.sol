20581e13f3b359d0476198894f699d2cd029a96b
ahmad1091 <ahmadtayeb91@gmail.com>
2021-03-07 10:39:20 +0200

    transfer function

--- contracts/my_token.sol
+++ contracts/my_token.sol
@@ -6,6 +6,9 @@ contract MyToken {
     string public symbol;
     string public standard = "ERC20";
     uint256 public totalSupply;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
     mapping(address => uint256) public balanceOf;
 
     constructor(
@@ -17,5 +20,18 @@ contract MyToken {
         balanceOf[msg.sender] = _initialSupply;
         name = _name;
         symbol = _symbol;
+    }
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(balanceOf[msg.sender] >= _value);
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
