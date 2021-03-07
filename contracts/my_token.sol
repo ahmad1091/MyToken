6de51802570a74d1cde38cc9018ae5c1fe8b05de
ahmad1091 <ahmadtayeb91@gmail.com>
2021-03-07 09:08:08 +0200

    add local variables to the constractor and set thim to the global variables

--- contracts/my_token.sol
+++ contracts/my_token.sol
@@ -2,9 +2,19 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract MyToken {
+    string public name;
+    string public symbol;
     uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
 
-    constructor() public {
-        totalSupply = 1000000;
+    constructor(
+        uint256 _initialSupply,
+        string memory _name,
+        string memory _symbol
+    ) public {
+        totalSupply = _initialSupply;
+        balanceOf[msg.sender] = _initialSupply;
+        name = _name;
+        symbol = _symbol;
     }
 }
