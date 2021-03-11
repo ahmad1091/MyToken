5ac050b3d5c738d7a435fa6f06fcdd1ddb7fca2c
ahmad1091 <ahmadtayeb91@gmail.com>
2021-03-11 08:30:02 +0200

    create the buy functions in the front end

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,16 @@ App = {
   web3Provider: null,
   contracts: [],
   account: "0x0",
+  loading: false,
+  tokenPrice: 1000000000000000,
+  tokensSold: 0,
+  tokensAvailable: 750000,
+
   init: () => {
     console.log("App initialized ..");
     return App.initWeb3();
   },
+
   initWeb3: () => {
     if (typeof web3 !== "undefined") {
       // If a web3 instance is already provided by Meta Mask.
@@ -20,6 +26,7 @@ App = {
     }
     return App.initContracts();
   },
+
   initContracts: () => {
     $.getJSON("MyTokenSale.json", (myTokenSale) => {
       App.contracts.MyTokenSale = TruffleContract(myTokenSale);
@@ -34,18 +41,99 @@ App = {
         App.contracts.MyToken.deployed().then((myToken) => {
           console.log("token Adress;;", myToken.address);
         });
+        App.listenForEvents();
+
         return App.render();
       });
     });
   },
+
+  listenForEvents: () => {
+    App.contracts.MyTokenSale.deployed().then((instance) => {
+      instance
+        .Sell(
+          {},
+          {
+            fromBlock: 0,
+            toBlock: "latest",
+          }
+        )
+        .watch((err, event) => {
+          console.log("event triggered", event);
+          App.render();
+        });
+    });
+  },
+
   render: () => {
+    if (App.loading) {
+      return;
+    }
+    App.loading = true;
+    var loader = $("#loader");
+    var content = $("#content");
+
+    loader.show();
+    content.hide();
+
     web3.eth.getCoinbase((err, account) => {
       if (!err) {
-        console.log(account);
         App.account = account;
         $("#accountAddress").html("Your Account:" + account);
       }
     });
+
+    App.contracts.MyTokenSale.deployed()
+      .then((instance) => {
+        myTokenSaleInstance = instance;
+        return myTokenSaleInstance.tokenPrice();
+      })
+      .then((tokenPrice) => {
+        App.tokenPrice = tokenPrice;
+        $(".token-price").html(
+          web3.fromWei(App.tokenPrice, "ether").toNumber()
+        );
+        return myTokenSaleInstance.tokenSold();
+      })
+      .then((tokensSold) => {
+        App.tokensSold = tokensSold.toNumber();
+        $(".tokens-sold").html(App.tokensSold);
+        $(".tokens-available").html(App.tokensAvailable);
+
+        var progressPercent = (App.tokensSold / App.tokensAvailable) * 100;
+        $("#progress").css("width", progressPercent + "%");
+        App.contracts.MyToken.deployed()
+          .then((instance) => {
+            myTokenInstance = instance;
+            return myTokenInstance.balanceOf(App.account);
+          })
+          .then((balance) => {
+            $(".my-balance").html(balance.toNumber());
+            // .done(() => {
+            App.loading = false;
+            loader.hide();
+            content.show();
+            // });
+          });
+      });
+  },
+
+  buyTokens: () => {
+    $("#content").hide();
+    $("#loader").show();
+    var numberOfTokens = $("#numberOfTokens").val();
+    App.contracts.MyTokenSale.deployed()
+      .then((instance) => {
+        return instance.buyTokens(numberOfTokens, {
+          from: App.account,
+          value: numberOfTokens * App.tokenPrice,
+          gas: 500000,
+        });
+      })
+      .then((result) => {
+        console.log("Tokens bought");
+        $("form").trigger("reset");
+      });
   },
 };
 
